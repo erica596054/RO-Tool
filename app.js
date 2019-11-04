9f14e4937798a10a9dc5e0be4d6fc1a4f31a4ef8
erica596054 <56697187+erica596054@users.noreply.github.com>
2019-11-04 14:03:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,10 @@
-// When the DOM has loaded, run the function load
+// When the DOM has load, run the function load
 document.addEventListener("DOMContentLoaded", load);
 
-// Grab the input box, the buttons and paragraph
-const input = document.querySelector("#refreshTime");
-const btn = document.querySelector("#btn");
-const btnStop = document.querySelector("#btn-stop");
-const para = document.querySelector("#info");
-
-// Create refresh variable
-let refreshInterval = null;
-
 function load() {
-  //initialise the tableau object
+  //initialise the tableau object, wait for the promise to return and grab the name of the dashboard
   tableau.extensions.initializeAsync().then(() => {
-    console.log("Tableau object loaded");
-    // Eventlistener on the start button
-    btn.addEventListener("click", () => {
-      if (input.value !== "") {
-        para.innerHTML = `Refresh is running every ${input.value} seconds`;
-        refreshInterval = setInterval(() => {
-          initTableau();
-        }, input.value * 1000);
-      } else {
-        para.innerHTML = "Please specify seconds till refresh";
-      }
-    });
-
-    btnStop.addEventListener("click", () => {
-      if (input.value !== "") {
-        clearInterval(refreshInterval);
-        console.log("Stopped the refresh..");
-        para.innerHTML = "Refresh is not running";
-      }
-    });
+    const dashboard = tableau.extensions.dashboardContent.dashboard;
+    console.log(dashboard.name);
   });
-}
-
-function initTableau() {
-  // get the Tableau elements
-  const dashboard = tableau.extensions.dashboardContent.dashboard;
-  const sheets = dashboard.worksheets[0];
-  const datasource = sheets.getDataSourcesAsync();
-  // refresh said data source
-  console.log("Refreshing Datasource..");
-  datasource.then(source => source[0].refreshAsync());
 }
